d954b76a554f5167cee09f38c4bf66f775c46804
Brancen Gregory <brancengregory@gmail.com>
2024-09-17 01:47:23 -0500

    Draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::{self, Write};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let port_name = "/dev/ttyACM0";
+    let baud_rate = 9_600;
+    
+    let port = serialport::new(port_name, baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open();
+    
+    match port {
+      Ok(mut port) => {
+        let mut serial_buf: Vec<u8> = vec![0; 1000];
+        println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+        loop {
+          match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+              io::stdout().write_all(&serial_buf[..t]).unwrap();
+              io::stdout().flush().unwrap();
+            },
+            Err(e) => eprintln!("{}", e)
+          }
+        }
+      },
+      Err(e) => {
+        eprintln!("Failed to open {}. Error: {}", port_name, e);
+      }
+    }
 }
